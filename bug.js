9f71e88fb690b65c29a2d2c75aed1d73a0f25ab4
BugHunter-X <askzenai@gmail.com>
2025-02-09 23:08:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo `Linking` API to handle deep links.  The `Linking.addEventListener` method is not being called correctly or the listener is not being properly removed.  The `Linking.getInitialURL` is returning null or undefined.
